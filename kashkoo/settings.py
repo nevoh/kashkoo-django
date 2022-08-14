86229b4e87f5cb821ebb92e6f91a898d9d850a44
nevoh <nevogola@gmail.com>
2022-08-14 05:11:00 +0300

    reverted back to sql lite db

--- kashkoo/settings.py
+++ kashkoo/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'kashkoo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'kashkoodb',
-        'USER': 'postgres',
-        'PASSWORD': 'R3bb3cc@',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
